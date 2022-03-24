cb239e4e6f92d801bd83729c98fb2381c39cc712
Jane Kim <jane.kim0717@gmail.com>
2022-03-23 20:53:27 -0400

    Completed Core Deliverables Retry

--- app/models/category.rb
+++ app/models/category.rb
@@ -6,9 +6,31 @@ class Category < ActiveRecord::Base
     end
 
     def self.highest_rated
-        binding.pry
         review = Review.order(:rating).last
         game = Game.find(review.game_id)
         game.category
     end
+
+    # def self.highest_rated
+    #     Review.all.max_by{|r|r.rating}.game.category
+    # end
+
+    # def self.highest_rated
+    #     highest_rating = Review.all.max_by { |r| r.rating }
+    #     game_w_highest_rating = highest_rating.game
+    #     category_w_highest_rating = game_w_highest_rating.category
+    #     Array(category_w_highest_rating)
+    # end
+
+    # def self.highest_rated
+    #     ratingArray = self.all.map do |category|
+    #         category.reviews.map do |review|
+    #             review.rating 
+    #         end
+    #     end
+    #     sumRating = ratingArray.map do |rating|
+    #         rating.sum   
+    #     end
+    #     Category.find(sumRating.each_with_index.max[1] + 1)
+    # end
 end
